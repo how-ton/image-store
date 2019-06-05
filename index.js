7f88d64a75e073089c75e4275b5090d7e506c16b
MattyR <matt.rushton@kaluza.com>
2019-06-05 20:31:34 +0100

    Upload and list storage bucket

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 // Imports the Google Cloud client library
 const {Storage} = require('@google-cloud/storage');
 
-// Creates a client
-const storage = new Storage();
 
+ListFiles = async (bucketName) => {
+    // Creates a client
+    const storage = new Storage();
+    const [files] = await storage.bucket(bucketName).getFiles();
+
+    console.log('Files:');
+    files.forEach(file => {
+        console.log(file.name);
+    });
+}
+
+ uploadFile = async (bucketName, filename) => {
+    // [START storage_upload_file]
+    // Imports the Google Cloud client library
+    const {Storage} = require('@google-cloud/storage');
+  
+    // Creates a client
+    const storage = new Storage();
+  
+    /**
+     * TODO(developer): Uncomment the following lines before running the sample.
+     */
+    // const bucketName = 'Name of a bucket, e.g. my-bucket';
+    // const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
+  
+    // Uploads a local file to the bucket
+    await storage.bucket(bucketName).upload(filename, {
+      // Support for HTTP requests made with `Accept-Encoding: gzip`
+      gzip: true,
+      // By setting the option `destination`, you can change the name of the
+      // object you are uploading to a bucket.
+      metadata: {
+        // Enable long-lived HTTP caching headers
+        // Use only if the contents of the file will never change
+        // (If the contents will change, use cacheControl: 'no-cache')
+        cacheControl: 'public, max-age=31536000',
+      },
+    });
+  
+    console.log(`${filename} uploaded to ${bucketName}.`);
+    // [END storage_upload_file]
+  }
+
+main = async () => {
+    const bucketName = process.env.IMG_STORE_BUCKET_NAME;
+    ListFiles(bucketName)
+    
+    await uploadFile(bucketName, process.env.IMG_STORE_UPLOAD_FOLDER + "/example-2.txt")
+    
+    ListFiles(bucketName)
+}
+ 
+main()
